420aed6496e3588bd7243db4bda4883f2e9171f6
throwAwayAccount695 <jako6981@tietgen.dk>
2020-03-10 11:21:14 +0100

    fix: fixed the $_SESSION error in menu bar.php, changed the tab spacing back on login.php and Menu Bar.php

--- Login.php
+++ Login.php
@@ -11,17 +11,17 @@
 	extract($_REQUEST);
 
 	if(isset($login)){
-  		if($eid=="" || $pass==""){
-  			$error= "<h4 style='color:red'>fill all details</h4>";  
-  		} else{
-  			$sql=mysqli_query($con,"select * from create_account where email='$eid' && password='$pass' ");
-    		if(mysqli_num_rows($sql)){
-   			$_SESSION['create_account_logged_in']=$eid;  
-   			header('location:Booking Form.php'); 
-   		} else{
-    			$error= "<h4 style='color:red'>Invalid login details</h4>"; 
-   		} 
-  		}	
+		if($eid=="" || $pass==""){
+			$error= "<h4 style='color:red'>fill all details</h4>";  
+		} else{
+			$sql=mysqli_query($con,"select * from create_account where email='$eid' && password='$pass' ");
+			if(mysqli_num_rows($sql)){
+			$_SESSION['create_account_logged_in']=$eid;  
+			header('location:Booking Form.php'); 
+			} else{
+				$error= "<h4 style='color:red'>Invalid login details</h4>"; 
+			} 
+		}	
 	}
 ?>
 <!DOCTYPE html>
@@ -40,10 +40,10 @@
 	<body style="margin-top:50px;">
 		<?php include('Menu Bar.php'); ?>
 		<div class="container-fluid"><!-- Primary Id-->
-  			<div class="container">
-    			<div class="row"><br>
-      			<div class="col-sm-4"></div>
-        			<div class="col-sm-4 text-center"style="box-shadow:2px 2px 2px;background-color:#f4ac41;"><br>
+  		<div class="container">
+    		<div class="row"><br>
+      		<div class="col-sm-4"></div>
+        	<div class="col-sm-4 text-center"style="box-shadow:2px 2px 2px;background-color:#f4ac41;"><br>
 						<h1 align="center"><b><font style="font-family: 'Libre Baskerville', serif;text-shadow:3px 3px #000;">User Login ?</font></b></h1>
 						<img src="image/clipart/login-user-icon.png" class="img-circle" alt="Bird" width="130" height="120">
 						<?php echo @$error; ?>
@@ -60,7 +60,7 @@
 								<a href="Registation form.php">Create an Account</a>
 							</div>
 						</form><br>
-        			</div>
+        		</div>
     			</div><br>
   			</div>
 		</div>
