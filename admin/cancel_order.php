20749e1912a2f2e58c89ca06e03057a966581d80
throwAwayAccount695 <jako6981@tietgen.dk>
2020-06-03 12:19:23 +0200

    [Refactored]: cancel_order.php has been refactored

--- admin/cancel_order.php
+++ admin/cancel_order.php
@@ -1,9 +1,7 @@
 <?php 
-include('../connection.php');
-$oid=$_GET['booking_id'];
-$q=mysqli_query($con,"delete from  room_booking_details where id='$oid' ");
-if($q)
-{
+require_once($_SERVER['DOCUMENT_ROOT'] . '/hotel_management/classes/Display.php');
+
+$id = $_GET['booking_id'];
+$display->delete_row("room_booking_details", array("id" => $id));
 header('location:dashboard.php?option=booking_details');
-}
 ?>
