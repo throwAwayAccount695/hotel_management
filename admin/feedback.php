4918039cdb4e71977ed6dfbe2cd7822d382c77d8
throwAwayAccount695 <jako6981@tietgen.dk>
2020-06-03 12:40:39 +0200

    [Refactor]: feedback.php and feedback_delete.php has been fefactored

--- admin/feedback.php
+++ admin/feedback.php
@@ -1,9 +1,7 @@
 <script>
-	function delFeedback(id)
-	{
-		if(confirm("You want to delete this Feedback ?"))
-		{
-		window.location.href='delete_feedback.php?id='+id;	
+	function delFeedback(id){
+		if(confirm("You want to delete this Feedback ?")){
+			window.location.href='delete_feedback.php?id=' + id;	
 		}
 	}
 </script>
@@ -17,27 +15,17 @@
 		<th>Message</th>
 		<th>Delete</th>
 	</tr>
-<?php 
-$i=1;
-$sql=mysqli_query($con,"select * from feedback");
-while($res=mysqli_fetch_assoc($sql))
-{
-$id=$res['id'];	
-$name=$res['name'];
-$email=$res['email'];
-$mobile	=$res['mobile'];
-$message=$res['message'];
-?>
-<tr>
-		<td><?php echo $i;$i++; ?></td>
-		<td><?php echo $res['name']; ?></td>
-		<td><?php echo $res['email']; ?></td>
-		<td><?php echo $res['mobile']; ?></td>
-		<td><?php echo $res['message']; ?></td>
-		<td><a href="#"onclick="delFeedback('<?php echo $id; ?>')"><span class="glyphicon glyphicon-remove"style='color:red'></span></a></td>
+	<?php 
+		$res = $display->select_all("feedback");
+		for($i = 0; $i < count($res); $i++) :
+	?>
+	<tr>
+		<td><?= $i + 1; ?></td>
+		<td><?= $res[$i]['name']; ?></td>
+		<td><?= $res[$i]['email']; ?></td>
+		<td><?= $res[$i]['mobile']; ?></td>
+		<td><?= $res[$i]['message']; ?></td>
+		<td><a href="#"onclick="delFeedback('<?= $res[$i]['id']; ?>')"><span class="glyphicon glyphicon-remove"style='color:red'></span></a></td>
 	</tr>	
-<?php 	
-}
-?>	
-	
+	<?php endfor; ?>	
 </table>
--- admin/feedback_delete.php
+++ admin/feedback_delete.php
@@ -1,13 +1,7 @@
 <?php 
-include('../connection.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/hotel_management/classes/Display.php');
 
-$id=$_GET['id'];
-
-$sql=mysqli_query($con,"select * from feedback where id='$id'");
-$res=mysqli_fetch_assoc($sql);
-if(mysqli_query($con,"delete from feedback where id='$id'"))
-{
+$id = $_GET['id'];
+$display->delete_row("feedback", array("id" => $id));
 header('location:dashboard.php?option=feedback');	
-}
-
 ?>
